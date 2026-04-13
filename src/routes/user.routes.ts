b5cb104305c2bd4ad334615d4f17ab2cd43afc8d
caiomarionpb <caiompbueno@gmail.com>
2026-04-13 03:05:52 -0300

    route correction

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { getProfile } from '../controllers/user.controller';
+import { updateProfile } from '../controllers/auth.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-// rota protegida
+// rotas protegidas
 router.get('/profile', authMiddleware, getProfile);
+router.put('/profile', authMiddleware, updateProfile);
 
 export default router; // ✅ precisa ser default
--- src/server.ts
+++ src/server.ts
@@ -16,10 +16,6 @@ app.use(express.urlencoded({ extended: true })); // opcional: lê form-data
 app.use('/api/auth', authRoutes);   // login: /api/auth/login
 app.use('/api/users', userRoutes);  // profile: /api/users/profile
 
-// Rotas
-app.use('/api/', userRoutes);
-app.use('/api/', authRoutes);
-
 app.get('/api/teste', (req, res) => {
   res.send('teste ok');
 });
